946818d448c2bb19c7aba58c48d347b92198b67a
nazir4116 <nazirismail306@gmail.com>
2026-09-12 14:23:35 +0100

    Add 'type' field to Note schema

--- src/models/Note.js
+++ src/models/Note.js
@@ -6,6 +6,7 @@ const noteSchema = new mongoose.Schema(
     title: { type: String, default: '' },
     content: { type: String, default: '' },
     fileUrl: { type: String, default: '' },
+    type: { type: String, enum: ['text', 'photo'], default: 'text' },
     code: { type: String, required: true, unique: true, index: true },
   },
   { timestamps: true }
